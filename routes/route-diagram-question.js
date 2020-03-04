4af6f4928ec7d351b4f949cb18792b0e21aee45c
takttk <59160177@go.buu.ac.th>
2020-03-04 11:16:22 +0700

    manage diagram

--- routes/route-diagram-question.js
+++ routes/route-diagram-question.js
@@ -2,6 +2,40 @@ var express = require('express');
 var router = express.Router();
 var authen = require('../utils/authen');
 var configString = require('../app').configString;
+var firestore = require('../configs/firebase-config').firestore;
+var setting = require('../public/javascripts/setting');
+/*
+ *Description: open diagram question.
+ *@version 2.0
+ *@author Thongthorn Karapakdee
+ *@since 6 December 2019
+ *@required node.js,ExpressJS.
+ */
+router.get('/add', authen, async function (req, res, next) {
+    let lang = req.cookies.lang;
+    const general = await setting.getSetting(lang)
+    let lesson = configString[lang].lesson.variable;
+    var data = {
+        layout: 'default',
+        navBar: true,
+        user: req.session.user,
+        //lesson: lesson,
+        //subLesson: lesson.subLesson.juice,
+        //element: configString[lang].element.variable.juice,
+        intro: configString[lang].intro,
+        recipe: configString[lang].recipe.variable.juice,
+        //required
+        setting: general.setting,
+        button: general.button,
+        elementDefault: configString[lang].element.general,
+        general: configString[lang].general,
+        achievementList: configString[lang].achievement,
+        errorMsg: configString[lang].error,
+        nextPage: '/lesson/variable/cereal'
+    };
+    res.render('manageDiagram/addDiagram', data);
+
+});
 
 /*
  *Description: open diagram question.
@@ -10,27 +44,71 @@ var configString = require('../app').configString;
  *@since 6 December 2019
  *@required node.js,ExpressJS.
  */
-router.get('/', authen, function (req, res, next) {
+router.get('/edit/:id', authen, async function (req, res, next) {
     let lang = req.cookies.lang;
+    const general = await setting.getSetting(lang)
     let lesson = configString[lang].lesson.variable;
-    var data = {
-        layout: 'default',
-        navBar: true,
-        user: req.session.user,
-        lesson: lesson,
-        subLesson: lesson.subLesson.juice,
-        element: configString[lang].element.variable.juice,
-        intro: configString[lang].intro,
-        recipe: configString[lang].recipe.variable.juice,
-        //required
-        elementDefault: configString[lang].element.general,
-        general: configString[lang].general,
-        achievementList: configString[lang].achievement,
-        errorMsg: configString[lang].error,
-        nextPage: '/lesson/variable/cereal'
-    };
-    res.render('addDiagram/addDiagram', data);
- 
-  });
+    let refQuestion = firestore.collection("Diagram");
+    refQuestion.doc(req.params.id).get().then(function (doc) {
+        var data = {
+            layout: 'default',
+            navBar: true,
+            user: req.session.user,
+            //lesson: lesson,
+            //subLesson: lesson.subLesson.juice,
+            //element: configString[lang].element.variable.juice,
+            intro: configString[lang].intro,
+            recipe: configString[lang].recipe.variable.juice,
+            HintEn: doc.data().HintEng,
+            HintTh: doc.data().HintTh,
+            NameEn: doc.data().NameEng,
+            NameTh: doc.data().NameTh,
+            level: doc.data().Level,
+            //required
+            setting: general.setting,
+            button: general.button,
+            elementDefault: configString[lang].element.general,
+            general: configString[lang].general,
+            achievementList: configString[lang].achievement,
+            errorMsg: configString[lang].error,
+            nextPage: '/lesson/variable/cereal'
+        };
+        res.render('manageDiagram/editDiagram', data);
+    });
+});
 
-  module.exports = router;
+async function getScore() {
+    let score = null;
+    const refscore = firestore.collection('System').doc('Score')
+    // /System/Score
+    await refscore.get().then(doc => {
+        if (!doc.exists) {
+            score = false
+        } else {
+            score = doc.data()
+        }
+    })
+        .catch(err => {
+            score = false
+        });
+    return score
+}
+
+async function getSetting(lang = 'en') {
+    // /System/Config/general/en
+    let general = [];
+    const refgeneral = firestore.collection('System').doc('Config')
+        .collection('general').doc(lang)
+    await refgeneral.get().then(doc => {
+        if (!doc.exists) {
+            general.push(false)
+        } else {
+            general.push(doc.data())
+        }
+    })
+        .catch(err => {
+            general.push(false)
+        });
+    return general[0]
+}
+module.exports = router;
--- routes/route-diagram.js
+++ routes/route-diagram.js
@@ -11,36 +11,85 @@ var setting = require('../public/javascripts/setting');
  *@since 6 December 2019
  *@required node.js,ExpressJS.
  */
-router.get('/:id', authen,async function (req, res, next) {
-    
+router.get('/:id', authen, async function (req, res, next) {
     let lang = req.cookies.lang;
     const general = await setting.getSetting(lang)
     //let lesson = configString[lang].lesson.logic;
     let refQuestion = firestore.collection("Diagram");
-    let questionData;
+    var arrAnswer = [];
+    let keyId = 0;
+    const score = await getScore();
+    refQuestion.doc(req.params.id).collection("answer").get().then(function (Doc) {
+        keyId = Doc.size;
+        for (let index = 1; index <= keyId; index++) {
+            refQuestion.doc(req.params.id).collection("answer").doc(index.toString()).get().then(function (doc2) {
+                arrAnswer.push(doc2.data().answerStep)
+            });
+        }
+        console.log("Answer => ", arrAnswer);
+    })
+
     refQuestion.doc(req.params.id).get().then(function (doc) {
-
-        console.log(questionData)
-        var data = {
-            layout: 'default',
-            navBar: true,
-            user: req.session.user,
-            //lesson: lesson,
-            //subLesson: lesson.subLesson.advance,
-            question: doc.data().answer,
-            questionHint: doc.data().Hint,
-            questionName: doc.data().Name,
-            length: doc.data().answer.length,
-            setting: general.setting,
-            button: general.button,
-            elementsString: configString[lang].element.general,
-            general: configString[lang].general,
-            achievementList: configString[lang].achievement,
-            errorMsg: configString[lang].error,
-            //nextPage: '/lesson/decision/coffee'
-        };
-        res.render('newDiagram/diagramById', data);
-
+        refQuestion.doc(req.params.id).collection("answer").doc("1").get().then(function (doc3) {
+            var data = {
+                lang: lang,
+                layout: 'default',
+                navBar: true,
+                user: req.session.user,
+                question: doc3.data().answerStep,
+                questionHintEng: doc.data().HintEng,
+                questionNameEng: doc.data().NameEng,
+                questionHintTh: doc.data().HintTh,
+                questionNameTh: doc.data().NameTh,
+                answerStep: arrAnswer,
+                length: doc3.data().answerStep.length,
+                maxScore: score.maxScore,
+                minScore: score.minScore,
+                setting: general.setting,
+                button: general.button,
+                elementsString: configString[lang].element.general,
+                general: configString[lang].general,
+                achievementList: configString[lang].achievement,
+                errorMsg: configString[lang].error,
+                //nextPage: '/lesson/decision/coffee'
+            };
+            res.render('newDiagram/diagramById', data);
+        });
     });
 });
+
+async function getScore(){
+    let score = null;
+    const refscore = firestore.collection('System').doc('Score')
+    // /System/Score
+    await refscore.get().then(doc => {
+      if (!doc.exists) {
+        score = false
+      } else {
+        score = doc.data()
+      }
+    })
+    .catch(err => {
+      score = false
+    });
+    return score
+  }
+
+  async function getSetting(lang='en'){
+    // /System/Config/general/en
+    let general = [];
+    const refgeneral = firestore.collection('System').doc('Config')
+                        .collection('general').doc(lang)
+    await refgeneral.get().then(doc => {
+      if (!doc.exists) {
+        general.push(false)
+      } else {
+        general.push(doc.data())
+      }
+    })
+    .catch(err => {
+      general.push(false)
+    });
+    return general[0]
+  }
 module.exports = router;
